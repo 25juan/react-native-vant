0ce5d1f7aecb1c96df6860c323e2afd59f5d95aa
25juan <1216181067@qq.com>
2022-06-01 20:31:49 +0800

    feat: 修改ali镜像

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { NativeModules, Platform } from 'react-native';
 
+
+
 const LINKING_ERROR =
   `The package 'react-native-vant' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
