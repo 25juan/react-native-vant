8a95fc797d90829505dce283f54db33f5137d870
25juan <1216181067@qq.com>
2022-06-01 20:47:09 +0800

    feat: 添加flex组件

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,11 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-vant';
+import { View } from 'react-native';
+import { Flex } from 'react-native-vant';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View>
+      <Flex>11</Flex>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
